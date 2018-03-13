2fc6bbd3b46813785cb154457565b50ad855ddb3
Roarke Catubig <roarkecatubig@gmail.com>
2018-03-12 22:32:25 -0700

    Progress Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,22 +23,35 @@ $("#add-train").on("click", function() {
     destination = $("#destination-input").val();
     trainTime = $("#time-input").val();
     frequency = $("#frequency-input").val();
-
-    database.ref().push({
+    trainsRef = database.ref("/trains");
+    var key = trainsRef.push().key;
+    console.log(key);
+    var update = {};
+    update[key] = {
         name: name,
         destination: destination,
-        trainArrivalTime: trainTime,
-        frequency: frequency
-    });
+        frequency: frequency,
+        trainArrivalTime: trainTime
 
-    var trainIndex = {};
+    };
+    var result = trainsRef.update(update);
 
-    trainIndex["Name"] = name;
-    trainIndex["Destination"] = destination;
-    trainIndex["Train Time"] = trainTime;
-    trainIndex["Frequency"] = frequency;
+    // database.ref("/trains").push({
+    //     name: name,
+    //     destination: destination,
+    //     frequency: frequency,
+    //     trainArrivalTime: trainTime
 
-    trains.push(trainIndex);
+    // });
+
+    // var trainIndex = {};
+
+    // trainIndex["Name"] = name;
+    // trainIndex["Destination"] = destination;
+    // trainIndex["Train Time"] = trainTime;
+    // trainIndex["Frequency"] = frequency;
+
+    // trains.push(trainIndex);
 
     // var newTable = $("#data-view");
 
@@ -50,6 +63,8 @@ $("#add-train").on("click", function() {
     // $("#data-view").append(newTable)
 
     database.ref().on("value", function(snapshot) {
+        trains = [];
+        trains.push(snapshot.val());
         $("#data-view").empty();
         // var dataStuff = snapshot.val();
         // var newData = snapshot.val();
@@ -59,15 +74,35 @@ $("#add-train").on("click", function() {
             // var newTableData = $("<td>").text(dataSet[property1]);
             // newTable.append(newTableData);
         // }
-        snapshot.forEach(function (childSnap) {
-            var newTable = $("<tbody>");
-            var dataRow = childSnap.val();
-            for(property1 in dataRow) {
-                var newTableData = $("<td>").text(dataRow[property1]);
-                newTable.append(newTableData);
-            };
-            $("#data-view").append(newTable)
-        });
+        var newTable = $("<tbody>");
+
+        var lookupItem = trains[0];
+
+        for (row in lookupItem) {
+            var newRow = $("<tr>")
+            var nameDis = $("<td scope='col'>").text(row[name]);
+            newRow.append(nameDis);
+            var destinationDis = $("<td scope='col'>").text(row[destination]);
+            newRow.append(destinationDis);
+            var frequencyDis = $("<td scope='col'>").text(lookupItem[frequency]);
+            newRow.append(frequencyDis);
+
+            newTable.append(newRow)
+            
+        }
+
+        $("#data-view").append(newTable)
+
+        // snapshot.forEach(function (childSnap) {
+            
+        //     var newTable = $("<tbody>");
+        //     var dataRow = childSnap.val();
+        //     for(property1 in dataRow) {
+        //         var newTableData = $("<td scope='col'>").text(dataRow[property1]);
+        //         newTable.append(newTableData);
+        //     };
+        //     $("#data-view").append(newTable)
+        // });
     
     });
 });
