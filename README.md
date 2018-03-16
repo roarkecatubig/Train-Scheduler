f9bd4fb7461dc4dbb757fac525907473a7b7c1c7
Roarke Catubig <roarkecatubig@gmail.com>
2018-03-16 09:57:22 -0700

    Project completed and styled

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Train-Scheduler
+
+This project is designed to take user input as far as the train name, destination first occurrence of the day when the specified train arrives and the frequency the train arrives at a station. 
+
+After the user fills out the appropriate fields and submits the form, the table automatically appends the user inputted information along with the next available train time and the current wait time until that train. User data is inputted into a firebase database and the site pulls object properties from the firebase database to display train informationThis project incorporates the moment.js script file to more easily access real time data and format at moments in time as needed for the purposes of this project. 
+
+Note: This site does not have incorporated functionality to update wait times and next available train in as time gradually goes on during the day. The site only presents accurate information at that moment in time. Refreshing the page will not display updated train and wait times. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,109 +10,87 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var trainsRef = database.ref();
 var name = "";
 var destination = "";
 var trainTime = "";
+var convertedTT = "";
+var currentTime = "";
+var timeDiff = "";
+var tRemainder ="";
+var timeWait = "";
+var nextTT = "";
+var nextTTF = "";
 var frequency = "";
-// var trainsRef = database.ref().child("trains");
-// var key = trainsRef.push().key;
-
-// trainsRef.on("value", function(snapshot) {
-//     $("#data-view").empty();
-//     for (row in snapshot.val()) {
-//         var nameDis = snapshot.child("name").val();
-//         var destinationDis = snapshot.child("destination").val();
-//         var frequencyDis = snapshot.child("frequency").val();
-//         var newRow = $("<tr><td>" + nameDis + "</td><td>" + destinationDis + "</td><td>" + frequencyDis + "</td></r>");
-//         $("#data-view").append(newRow)
-//     }
-
-    
-// });
+var newDataPoint = "";
 
 $("#add-train").on("click", function() {
-    $("#data-view").empty();
+    
     event.preventDefault();
-
+    
+    // Form input stored to variables.
     name = $("#name-input").val();
     destination = $("#destination-input").val();
     trainTime = $("#time-input").val();
     frequency = $("#frequency-input").val();
-    trainsRef = database.ref("/trains");
-    var key = trainsRef.push().key;
-    console.log(key);
-    var update = {};
-    update[key] = {
+
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+
+    // Convert inputted "trainTime" variable into readable format.
+    convertedTT = moment(trainTime, "HH:mm").format("HH:mm");
+    console.log("First Train Time: " + convertedTT);
+
+    // Getting current time.
+    currentTime = moment().format("HH:mm");
+    console.log("Current Time: " + currentTime);
+
+    // timeDiff = moment(currentTime, "HH:mm").diff(moment(convertedTT, "HH:mm"));
+    timeDiff = moment().diff(moment(convertedTT, "HH:mm"), "minutes")
+    console.log("Time difference: " + timeDiff);
+
+    tRemainder = timeDiff % frequency;
+    console.log("tRemainder: " + tRemainder)
+
+    timeWait = frequency - tRemainder;
+    console.log("Wait Time: " + timeWait);
+
+    nextTT = moment().add(timeWait, "minutes");
+    console.log("(Unformatted) Next Train Time: " + timeWait);
+
+    nextTTF = moment(nextTT).format("hh:mm a")
+    console.log("(Formatted) Next Train Time: " + nextTTF)
+
+    // Difference between the first "trainTime" variable and current time
+    newDataPoint = trainsRef.push({
         name: name,
         destination: destination,
+        trainTime: trainTime,
         frequency: frequency,
-        trainArrivalTime: trainTime
-
-    };
-    var result = trainsRef.update(update);
-
-    trainsRef.on("value", function(snapshot) {
-        $("#data-view").empty();
-        var nameDis = snapshot.child(key).child("name").val();
-        var destinationDis = snapshot.child(key).child("destination").val();
-        var frequencyDis = snapshot.child(key).child("frequency").val();
-
-        var newRow = $("<tr><td>" + nameDis + "</td><td>" + destinationDis + "</td><td>" + frequencyDis + "</td></r>");
-
-        $("#data-view").append(newRow)
-
-        // snapshot.forEach(function (childSnap) {
-            
-        //     var newTable = $("<tbody>");
-        //     var dataRow = childSnap.val();
-        //     for(property1 in dataRow) {
-        //         var newTableData = $("<td scope='col'>").text(dataRow[property1]);
-        //         newTable.append(newTableData);
-        //     };
-        //     $("#data-view").append(newTable)
-        // });
-    
+        currentTime: currentTime,
+        nextTrain: nextTTF,
+        waitTime: timeWait
     });
+
+    return false;
 });
 
-  
-        
+
+trainsRef.on("child_added", function(childSnapshot) {
+    var nameDis = childSnapshot.val().name;
+    var destinationDis = childSnapshot.val().destination;
+    var frequencyDis = childSnapshot.val().frequency;
+    var nextTrainDis = childSnapshot.val().nextTrain;
+    var waitDis = childSnapshot.val().waitTime;
+
+    var newRow = $("<tr><td>" + nameDis + "</td><td>" + destinationDis + "</td><td>" + frequencyDis + "</td><td>" + nextTrainDis + "</td><td>" + timeWait + "<td></tr>");
+    // var newRow = $("<tr><td>" + nameDis + "</td><td>" + destinationDis + "</td><td>" + frequencyDis + "</td><td>" + nextTrainDis + "</td><td>" + waitDis + "</td></tr>");
+
+    $("#data-view").append(newRow)
 
 
+});
 
 
-//     database.ref().on("value", function(snapshot) {
-//     $("#name-display").text(snapshot.val().name);
-//     $("#destination-display").text(snapshot.val().destination);
-//     $("#frequency-display").text(snapshot.val().frequency);
-//     $("#time-display").text(snapshot.val().trainTime);
-
-//         }, function(errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   });
-
-    // displayTable()
-
-
-// function displayTable() {
-
-//     for (i=0; i < trains.length; i++) {
-//         var newTable = $("<tbody>")
-//         for (property1 in trains[i]) {
-//             var newTableData = $("<td>").text(property1);
-//             newTable.append(newTableData);
-//         }
-//     }
-
-//     $("#data-view").append(newTable)
-// }
-
-// database.ref().on("value", function(snapshot) {
-//     $("#name-display").text(snapshot.val().name);
-//     $("#destination-display").text(snapshot.val().destination);
-//     $("#frequency-display").text(snapshot.val().frequency);
-//     $("#time-display").text(snapshot.val().trainTime);
-
-//         }, function(errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   });
